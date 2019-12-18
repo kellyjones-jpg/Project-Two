c5963bf1805a3426df7eb9f9e20bf7a3822e60f7
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2019-12-17 20:58:40 -0700

    updating server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 var express = require("express");
 
-var db = require("./models");
+// var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -10,8 +10,6 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-
-
 
 // Routes
 require("./routes/apiRoutes")(app);
@@ -26,14 +24,15 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+// db.sequelize.sync(syncOptions).then(function() {
+// });
+
+app.listen(PORT, function() {
+  console.log(
+    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+    PORT,
+    PORT
+  );
 });
 
 module.exports = app;
